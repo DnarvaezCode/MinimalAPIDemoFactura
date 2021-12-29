fa476b509237f1307e18ca8ab214f7a4992e8da0
Dnarvaez2590 <douglasnarvaezing@gmail.com>
2021-12-29 14:32:49 -0600

    Validación modelo

--- APIFacturaV1/DTOs/ClienteDTO.cs
+++ APIFacturaV1/DTOs/ClienteDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace APIFacturaV1.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIFacturaV1.DTOs
 {
     public class ClienteDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Campo requerido.")]
         public string Nombres { get; set; }
         public string Apellidos { get; set; }
         public string Direccion { get; set; }
